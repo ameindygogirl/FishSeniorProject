56fae2ef3ab8af1676ea89a0a550f67a284ba591
thomasflores84 <thomas.flores84@eagles.ewu.edu>
2014-11-11 16:37:42 -0800

    UI Project Started
    This may conflict with Ralph's. I can just add what little I've done to
    his once I get it.

---
+++ SurveyApplication/SurveyApplication/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace SurveyApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(149, 233);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(449, 39);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "What would you like to do?";
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(83)))), ((int)(((byte)(148)))));
+            this.ClientSize = new System.Drawing.Size(708, 519);
+            this.Controls.Add(this.label2);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
