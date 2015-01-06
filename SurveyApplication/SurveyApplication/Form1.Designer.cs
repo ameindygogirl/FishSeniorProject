c034075cc87d9ff389ee34c7e62839c03bac8071
thomasflores84 <thomas.flores84@eagles.ewu.edu>
2015-01-06 15:21:25 -0800

    A few notes have been addressed.

--- SurveyApplication/SurveyApplication/Form1.Designer.cs
+++ SurveyApplication/SurveyApplication/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.label2 = new System.Windows.Forms.Label();
+            this.btnCreelSurvey = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label2
@@ -41,13 +42,32 @@
             this.label2.TabIndex = 0;
             this.label2.Text = "What would you like to do?";
             // 
+            // btnCreelSurvey
+            // 
+            this.btnCreelSurvey.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(158)))), ((int)(((byte)(235)))));
+            this.btnCreelSurvey.BackgroundImage = global::SurveyApplication.Properties.Resources.Creel_Icon;
+            this.btnCreelSurvey.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnCreelSurvey.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCreelSurvey.FlatAppearance.BorderColor = System.Drawing.Color.Cyan;
+            this.btnCreelSurvey.FlatAppearance.BorderSize = 0;
+            this.btnCreelSurvey.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(123)))), ((int)(((byte)(185)))));
+            this.btnCreelSurvey.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCreelSurvey.ForeColor = System.Drawing.Color.Transparent;
+            this.btnCreelSurvey.Location = new System.Drawing.Point(254, 298);
+            this.btnCreelSurvey.Name = "btnCreelSurvey";
+            this.btnCreelSurvey.Size = new System.Drawing.Size(106, 103);
+            this.btnCreelSurvey.TabIndex = 1;
+            this.btnCreelSurvey.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(83)))), ((int)(((byte)(148)))));
             this.ClientSize = new System.Drawing.Size(708, 519);
+            this.Controls.Add(this.btnCreelSurvey);
             this.Controls.Add(this.label2);
             this.ForeColor = System.Drawing.Color.White;
             this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -57,6 +77,7 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCreelSurvey;
     }
 }
 
